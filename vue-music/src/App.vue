b686b09bdc33fdb62904e45ac097adc1bb8e349e
jeff <jeffxiao@techno-vision.cn>
2019-12-03 20:31:16 +0800

    20191203

--- vue-music/src/App.vue
+++ vue-music/src/App.vue
@@ -1,23 +1,22 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
-    <router-view/>
+    <!-- <img src="./assets/logo.png">
+    <router-view/>-->
+    <m-header></m-header>
   </div>
 </template>
 
-<script>
+<script type="text/ecmascript-6">
+import MHeader from 'components/m-header/m-header'
 export default {
-  name: 'App'
+  components: { MHeader }
 }
 </script>
 
-<style>
+<style lang="stylus" scoped rel="stylesheet/stylus">
+@import './common/stylus/variable'
+
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  color: $color-theme;
 }
 </style>
