3df3c09beb925d272519f72a50320cdbcb50db7f
jeff <jeffxiao@techno-vision.cn>
2019-12-05 18:08:31 +0800

    20191205

---
+++ vue-music/src/api/config.js
@@ -0,0 +1,9 @@
+export const commonParams = {
+    g_tk: 5381,
+    inCharset: 'utf-8',
+    outCharset: '',
+    notice: 0,
+    format: 'jsonp'
+}
+
+export const options = {}
--- vue-music/src/api/recommend.js
+++ vue-music/src/api/recommend.js
@@ -1,5 +1,5 @@
 import jsonp from 'common/js/jsonp'
 
 export function getRecommend() {
-
+    const url = 'http://localhost:9091/test'
 }
