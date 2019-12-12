cb5d8d136ca74e8c99aa0a988d814e0b04e8e261
jeff <jeffxiao@techno-vision.cn>
2019-12-12 17:30:36 +0800

    20191212

--- travel/src/pages/home/components/Icons.vue
+++ travel/src/pages/home/components/Icons.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="icons">
-    <swiper :options="swiperOption">
+    <swiper>
       <swiper-slide v-for="(page, index) of pages" :key="index">
         <div class="icon" v-for="item of page" :key="item.id">
           <div class="icon-img">
@@ -18,10 +18,6 @@ export default {
   name: 'HomeIcons',
   data() {
     return {
-      swiperOption: {
-        pagination: '.swiper-pagination',
-        loop: true
-      },
       iconList: [
         {
           id: '0001',
@@ -100,36 +96,37 @@ export default {
 @import '~styles/varibles.styl';
 @import '~styles/mixins.styl';
 .icons >>> .swiper-container
-    height : 0
-    padding-bottom : 50%
+  height : 0
+  padding-bottom : 50%
 .icons
-    overflow : hidden
-    height : 0
-    padding-bottom: 50%
-    .icon
-        position : relative
-        float : left
-        height : 0
-        width : 25%
-        padding-bottom: 25%
-        .icon-img
-            position : absolute
-            top : 0
-            left : 0
-            right : 0
-            bottom : .44rem
-            .icon-img-content
-                display : block
-                margin : 0 auto
-                height : 100%
-        .icon-desc
-            position : absolute
-            left : 0
-            right : 0
-            bottom : 0
-            height : .44rem
-            text-align : center
-            line-height : .44rem
-            color : $darkTextColor
-            ellipsis ()
+  margin-top : .1rem
+  overflow : hidden
+  height : 0
+  padding-bottom: 50%
+  .icon
+      position : relative
+      float : left
+      height : 0
+      width : 25%
+      padding-bottom: 25%
+      .icon-img
+          position : absolute
+          top : 0
+          left : 0
+          right : 0
+          bottom : .44rem
+          .icon-img-content
+              display : block
+              margin : 0 auto
+              height : 100%
+      .icon-desc
+          position : absolute
+          left : 0
+          right : 0
+          bottom : 0
+          height : .44rem
+          text-align : center
+          line-height : .44rem
+          color : $darkTextColor
+          ellipsis ()
 </style>
---
+++ travel/src/pages/home/components/Weekend.vue
@@ -0,0 +1,78 @@
+<template>
+  <div>
+    <div class="title">热销推荐</div>
+    <ul>
+      <li
+        class="item border-bottom"
+        v-for="item of recommendList"
+        :key="item.id"
+      >
+        <div class="item-img-wrapper">
+          <img class="item-img" :src="item.imgUrl" />
+        </div>
+        <div class="item-info">
+          <p class="item-title">{{ item.title }}</p>
+          <p class="item-desc">{{ item.desc }}</p>
+        </div>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HomeWeekend',
+  data() {
+    return {
+      recommendList: [
+        {
+          id: '0001',
+          imgUrl:
+            'https://imgs.qunarzz.com/vs_ceph_vs_tts/249448a3-5bd8-4f0c-bac2-aed51a0d7e00.jpg_r_390x260x90_439fee08.jpg',
+          title: '超级测试',
+          desc: '测试机器性能'
+        },
+        {
+          id: '0002',
+          imgUrl:
+            'https://imgs.qunarzz.com/vs_ceph_vs_tts/249448a3-5bd8-4f0c-bac2-aed51a0d7e00.jpg_r_390x260x90_439fee08.jpg',
+          title: '大连',
+          desc: '大连测试圣地'
+        },
+        {
+          id: '0003',
+          imgUrl:
+            'https://imgs.qunarzz.com/vs_ceph_vs_tts/249448a3-5bd8-4f0c-bac2-aed51a0d7e00.jpg_r_390x260x90_439fee08.jpg',
+          title: '大连',
+          desc: '大连测试圣地'
+        }
+      ]
+    }
+  }
+}
+</script>
+
+<style lang="stylus" scoped>
+@import '~styles/mixins.styl'
+.title
+  margin-top : .2rem
+  line-height : .8rem
+  background : #eee
+  text-indent : .2rem
+.item-img-wrapper
+  overflow : hidden
+  height : 0
+  padding-bottom : 33.9%
+  .item-img
+    width : 100%
+.item-info
+  padding : .1rem
+  .item-title
+    line-height : .54rem
+    font-size : .32rem
+    ellipsis()
+  .item-desc
+    line-height : .4rem
+    color : #ccc
+    ellipsis()
+</style>
