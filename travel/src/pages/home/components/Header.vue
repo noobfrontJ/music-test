bbd918bde3e748412452baab951a09f8a4122c1e
jeff <jeffxiao@techno-vision.cn>
2019-12-16 14:30:46 +0800

    20191216

--- travel/src/pages/home/components/Header.vue
+++ travel/src/pages/home/components/Header.vue
@@ -7,10 +7,12 @@
       <span class="iconfont">&#xe632;</span>
       输入城市/景点/游玩主题
     </div>
-    <div class="header-right">
-      {{ this.city }}
-      <span class="iconfont arrow-icon">&#xe64a;</span>
-    </div>
+    <router-link to='/city'>
+      <div class="header-right">
+        {{ this.city }}
+        <span class="iconfont arrow-icon">&#xe64a;</span>
+      </div>
+    </router-link>
   </div>
 </template>
 
@@ -27,12 +29,13 @@ export default {
 @import '~styles/varibles.styl'
 .header
   display : flex
-  height : .86rem
+  height : $headerHeight
   background : $bgColor
   color : #fff
   .header-left
       width : .64rem
       float : left
+      padding : 0.2rem 0rem 0rem 0.2rem
       .back-icon
           text-align: center
           font-size: .4rem
@@ -44,9 +47,10 @@ export default {
       background : #fff
       border-radius : .1rem
       color : #ccc
+      line-height : $headerHeight
   .header-right
       min-width: 1.04rem
-      padding: 0 .1rem
+      padding: 0.3rem .1rem
       float: right
       text-align: center
       color: #fff
