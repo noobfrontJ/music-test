fc0e788ad042f63cade79d7ed8f9042ed8db7d4d
jeff <jeffxiao@techno-vision.cn>
2019-12-18 19:47:00 +0800

    20191218

---
+++ travel/src/pages/detail/components/Header.vue
@@ -0,0 +1,30 @@
+<template>
+  <div>
+    <router-link tag="div" to="/" class="header-abs">
+      <span class="iconfont header-abs-back">&#xe624;</span>
+    </router-link>
+    <div class="header-fixed"></div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'DetailHeader'
+}
+</script>
+
+<style lang="stylus" scoped>
+.header-abs
+    position : absolute
+    left : .2rem
+    top : .3rem
+    width : .8rem
+    height : .8rem
+    line-height : .8rem
+    border-radius : .4rem
+    text-align: center;
+    background : rgba(0,0,0,.8)
+    .header-abs-back
+        color : #fff
+        font-size : .4rem
+</style>
