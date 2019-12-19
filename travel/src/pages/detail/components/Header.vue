5c78f67074aea25aa46adf8eaefe827501690be6
jeff <jeffxiao@techno-vision.cn>
2019-12-19 10:08:32 +0800

    20191219

--- travel/src/pages/detail/components/Header.vue
+++ travel/src/pages/detail/components/Header.vue
@@ -1,19 +1,52 @@
 <template>
   <div>
-    <router-link tag="div" to="/" class="header-abs">
+    <router-link tag="div" to="/" class="header-abs" v-show="showAbs">
       <span class="iconfont header-abs-back">&#xe624;</span>
     </router-link>
-    <div class="header-fixed"></div>
+    <div class="header-fixed" v-show="!showAbs" :style="opacityStyle">
+      <router-link to="/">
+        <div class="iconfont header-fixed-back">&#xe624;</div>
+      </router-link>
+      景点详情
+    </div>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'DetailHeader'
+  name: 'DetailHeader',
+  data() {
+    return {
+      showAbs: true,
+      opacityStyle: {
+        opacity: 0
+      }
+    }
+  },
+  methods: {
+    handlerScroll() {
+      const top = document.documentElement.scrollTop
+      if (top > 60) {
+        let opacity = top / 140
+        opacity = opacity > 1 ? 1 : opacity
+        this.opacityStyle = { opacity }
+        this.showAbs = false
+      } else {
+        this.showAbs = true
+      }
+    }
+  },
+  mounted() {
+    window.addEventListener('scroll', this.handlerScroll)
+  },
+  unmounted() {
+    window.removeEventListener('scroll', this.handleScroll)
+  }
 }
 </script>
 
 <style lang="stylus" scoped>
+@import '~styles/varibles.styl';
 .header-abs
     position : absolute
     left : .2rem
@@ -27,4 +60,24 @@ export default {
     .header-abs-back
         color : #fff
         font-size : .4rem
+.header-fixed
+    position : fixed
+    top : 0
+    left : 0
+    right : 0
+    overflow : hidden
+    height : $headerHeight
+    line-height : $headerHeight
+    text-align : center
+    color : #fff
+    background : $bgColor
+    font-size : .32rem
+    .header-fixed-back
+        position: absolute
+        top: 0
+        left: 0
+        width: .64rem
+        text-align: center
+        font-size: .4rem
+        color: #fff
 </style>
